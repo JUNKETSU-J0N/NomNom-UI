96207a19f851e17865614c06363d931e2db5e106
FranziAE <148230994+FranziAE@users.noreply.github.com>
2025-04-25 10:29:50 +0200

    ui 10 und 13 sidebar und startseite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,53 @@
 import {Routes} from '@angular/router';
-import {SwipeComponent} from './swipe/swipe.component';
 
-export const routes: Routes = [{
-  path: '',
-  pathMatch: 'full',
-  redirectTo: 'swipe'
-},
+import {SwipeComponent} from './swipe/swipe.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { ImpressumComponent } from './features/impressum/impressum.component';
+import { HelppageComponent } from './features/helppage/helppage.component';
+import { HistoryComponent } from './features/history/history.component';
+import { SettingsComponent } from './features/settings/settings.component';
+import { MyrecipesComponent } from './features/myrecipes/myrecipes.component';
+import { ShoppinglistComponent } from './features/shoppinglist/shoppinglist.component';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: DashboardComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'impressum',
+    component: ImpressumComponent
+  },
   {
     path: 'swipe',
     component: SwipeComponent
   },
-  // {
-  //   path: 'home',
-  //   component: HomeComponent
-  // }
+  {
+    path: 'help',
+    component: HelppageComponent
+  },
+  {
+    path: 'history',
+    component: HistoryComponent
+  },
+  {
+    path: 'list',
+    component: ShoppinglistComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  },
+  {
+    path: 'myrecipes',
+    component: MyrecipesComponent
+  },
+  { // wildcard route, alles was nicht definiert ist und eingegeben wird, führt wieder zu home
+    path: '**', redirectTo: 'home'
+  }
 ];
---
+++ src/app/features/shoppinglist/shoppinglist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppinglist',
+  imports: [],
+  templateUrl: './shoppinglist.component.html',
+  styleUrl: './shoppinglist.component.scss'
+})
+export class ShoppinglistComponent {
+
+}
