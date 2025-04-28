91bf5194fdbcb7e78239d5ec77a4d59d72113c2b
Lena <lena.pb@posteo.com>
2025-04-28 16:23:51 +0200

    MyRecipes Komponente hinzugefügt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {LayoutComponent} from './layout/layout.component';
+import { LayoutComponent } from './layout/layout.component';
 import { SwipeComponent } from './swipe/swipe.component';
+import { RouterModule } from '@angular/router';
+import { MyrecipesComponent } from './features/myrecipes/myrecipes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, SwipeComponent, LayoutComponent],
+  imports: [CommonModule, SwipeComponent, LayoutComponent, RouterModule, MyrecipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
