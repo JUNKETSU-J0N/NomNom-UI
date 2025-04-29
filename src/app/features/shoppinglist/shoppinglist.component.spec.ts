738537ed2112d69f1284f7c00a5e2431856e433d
JUNKETSU-J0N <jbrockmann752@gmail.com>
2025-04-29 22:08:48 +0200

    Test fixen nochmal XD

--- src/app/features/shoppinglist/shoppinglist.component.spec.ts
+++ src/app/features/shoppinglist/shoppinglist.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShoppingListComponent } from './shoppinglist.component';
+import {provideHttpClient} from '@angular/common/http';
 
 describe('ShoppingListComponent', () => {
   let component: ShoppingListComponent;
