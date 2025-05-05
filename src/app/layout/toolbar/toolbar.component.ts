3c9b0276760e5c3943c3c421c8129f4590a34363
Lena <lena.pb@posteo.com>
2025-05-05 14:56:24 +0200

    button für dark und light mode

--- src/app/layout/toolbar/toolbar.component.ts
+++ src/app/layout/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Output, OnInit} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -16,7 +16,7 @@ import {KeycloakService} from '../../utils/keycloak/keycloak.service';
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit{
   @Output() toggle = new EventEmitter<void>();
 
   constructor(
@@ -32,4 +32,26 @@ export class ToolbarComponent {
     this.keycloakService.accountManagement();
   }
 
+  isDarkTheme = false;
+
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme');
+    this.isDarkTheme = savedTheme === 'dark';
+
+    const html = document.documentElement;
+    html.classList.add(this.isDarkTheme ? 'theme-dark' : 'theme-light');
+  }
+
+  toggleTheme(): void {
+    this.isDarkTheme = !this.isDarkTheme;
+
+    const html = document.documentElement;
+    html.classList.remove(this.isDarkTheme ? 'theme-light' : 'theme-dark');
+    html.classList.add(this.isDarkTheme ? 'theme-dark' : 'theme-light');
+
+    localStorage.setItem('theme', this.isDarkTheme ? 'dark' : 'light');
+  }
+
+
 }
+
