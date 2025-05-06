ecfb18db7fc87df2d5c8b88711f1418762e9a0f9
Hamza <148235859+Sol-Harmony@users.noreply.github.com>
2025-05-06 16:15:32 +0200

    imports im spec.ts

--- src/app/features/recipe-detail/recipe-detail.component.spec.ts
+++ src/app/features/recipe-detail/recipe-detail.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecipeDetailComponent } from './recipe-detail.component';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Unit } from '../models/unit.enum';
+
+import { RecipeService } from '../../shared/services/recipe.service';
+import { Recipe } from '../../core/models/recipe.model';
 
 describe('RecipeDetailComponent', () => {
   let component: RecipeDetailComponent;
