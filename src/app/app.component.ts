5aaf6a36a5457a6521e8dafbad2c4a13354602dd
JUNKETSU-J0N <jbrockmann752@gmail.com>
2025-05-13 23:52:53 +0200

    User wird bei anmeldung geprüft ob diese in der Postgres DB vorhanden ist oder neu angelegt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { RouterModule } from '@angular/router';
+import {UserService} from './shared/services/user.service';
+import {KeycloakService} from './utils/keycloak/keycloak.service';
 
 @Component({
   selector: 'app-root',
@@ -15,4 +17,16 @@ import { RouterModule } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+constructor(
+  private userService: UserService,
+  private keycloak: KeycloakService
+) {}
+
+  ngOnInit(): void {
+    const userId = this.keycloak.userId;
+    console.log(userId)
+    this.userService.checkIfUserExists(userId).subscribe(() => {
+      console.log('sub block')
+    });
+  }
 }
